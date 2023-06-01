8cd051f3e5ac0fcfece3286666cf8c3353d039f3
Elandaza <135186318+Elandaza@users.noreply.github.com>
2023-05-31 22:59:17 -0500

    Update app.py
    Actualizar traduccion

--- app.py
+++ app.py
@@ -20,9 +20,37 @@ def home():
     if request.method == 'POST':
         text = request.form['text']
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
-        source_language = ''
-        translated_text = text
+        # Prepare the translation request
+        headers = {
+            'Ocp-Apim-Subscription-Key': cog_key,
+            'Ocp-Apim-Subscription-Region': cog_region,
+            'Content-Type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
 
+        body = [{
+            'text': text
+        }]
+
+        params = {
+            'api-version': '3.0',
+            'to': target_language
+        }
+
+        # Send the translation request
+        response = requests.post(
+            f"{translator_endpoint}/translate",
+            headers=headers,
+            params=params,
+            json=body
+        )
+
+        if response.status_code == 200:
+            translated_text = response.json()[0]['translations'][0]['text']
+        else:
+            translated_text = 'Translation failed'
+        source_language = ''
+        
         return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
     
     return render_template('home.html')
