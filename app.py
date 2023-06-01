e92808f769aeadb078952a9dcb748caa672e844f
Elandaza <135186318+Elandaza@users.noreply.github.com>
2023-06-01 11:08:22 -0500

    agrego funciones deteccion y traduccio

--- app.py
+++ app.py
@@ -22,6 +22,58 @@ def home():
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
         source_language = ''
         translated_text = text
+        
+        # inicio de coidgo deteccion de lenguaje
+        params = {
+            'api-version': '3.0'
+        }
+
+        headers = {
+            'Ocp-Apim-Subscription-Key': cog_key,
+            'Ocp-Apim-Subscription-Region': cog_region,
+            'Content-type': 'application/json'
+        }
+
+        body = [{
+            'text': text
+        }]
+
+        # Send the request and get response
+        request = requests.post(url, params=params, headers=headers, json=body)
+        response = request.json()
+
+        # Parse JSON array and get language
+        source_language = response[0]["language"]
+        #language = response[0]["language"]
+        
+        #Fin codigo deteccion de lenguaje
+
+        #inicio codigo traduccion
+        # Build the request
+        params = {
+            'api-version': '3.0',
+            'from': source_language,
+            'to': ['en']
+        }
+
+        headers = {
+            'Ocp-Apim-Subscription-Key': cog_key,
+            'Ocp-Apim-Subscription-Region': cog_region,
+            'Content-type': 'application/json'
+        }
+
+        body = [{
+            'text': text
+        }]
+
+        # Send the request and get response
+        request = requests.post(url, params=params, headers=headers, json=body)
+        response = request.json()
+
+        # Parse JSON array and get translation
+        translation = response[0]["translations"][0]["text"]
+        #Fin codigo de traducción
+
 
         return render_template('home.html', translated_text=translated_text,lang_detected=source_language)
     
